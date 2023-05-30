e8d144090566cab8dbca4f4616f22b89011c182c
Galdo <galdric.crabie1@hotmail.fr>
2023-05-31 00:49:57 +0200

    debug indexphp

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php include "html_partials/header.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <ul class="header">
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="index.php">Pierres</a></li>
+        <li><a href="index.php">Shop</a></li>
+        <li><a href="index.php">Games</a></li>
+    </ul>
 
 <div class="grand_tab">
     <div class="petit_tab"> XD </div> 
